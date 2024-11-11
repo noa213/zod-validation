9d40bb67df2b9a2837d2dbaf3d2fbb4a1c0e1991
noa <abmagori@gmail.com>
2024-11-11 14:25:13 +0200

    2

--- src/app/pages/validation/page.tsx
+++ src/app/pages/validation/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Validation from "@/app/components/validation";
+import Validation from "@/app/components/Validation";
 
 const page = () => {
   return <Validation />;
